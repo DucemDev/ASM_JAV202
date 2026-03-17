14e71c17a5e9a0221d23d1f97c44af2da898d22d
Huynh Le Duc Anh <huynhdem2609@gmail.com>
2026-03-17 12:00:08 +0700

    update ChangeInformationServlet.java

--- src/main/java/com/cafe/servlet/ChangeInformationServlet.java
+++ src/main/java/com/cafe/servlet/ChangeInformationServlet.java
@@ -1,5 +1,8 @@
 package com.cafe.servlet;
 
+import com.cafe.dao.UserDAO;
+import com.cafe.dao.UserDAOImpl;
+import com.cafe.entity.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,11 +13,14 @@ import java.io.IOException;
 
 @WebServlet({"/change-information", "changing"})
 public class ChangeInformationServlet extends HttpServlet {
+    UserDAO userDAO = new UserDAOImpl();
+    User user = userDAO.login("", "");
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (req.getRequestURI().endsWith("change-information")) {
             req.getRequestDispatcher("/WEB-INF/public/change-information.jsp").forward(req, resp);
-        }else{
+        } else {
             return;
         }
     }
@@ -22,5 +28,22 @@ public class ChangeInformationServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        if (verifyBeforeChange()==true) {
+            changeInformation();
+        }else {
+
+        }
+    }
+
+    void changeInformation() {
+
+    }
+
+    boolean verifyBeforeChange() {
+        if (user != null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
