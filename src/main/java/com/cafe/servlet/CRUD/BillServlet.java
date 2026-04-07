6bfcee909c7a44fa1288f037bf598b45e88e7436
My Ngoc <ngoctmts001713g@gmail.com>
2026-04-07 15:31:10 +0700

    update bill

---
+++ src/main/java/com/cafe/servlet/CRUD/BillServlet.java
@@ -0,0 +1,21 @@
+package com.cafe.servlet.CRUD;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet({"manager/bill","manger/billdetail"})
+public class BillServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
